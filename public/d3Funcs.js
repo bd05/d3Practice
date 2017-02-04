0933552a05e32f011c3619da1c25890d24b46e1e
bd05 <berdao.09@gmail.com>
2017-02-04 03:03:00 -0800

    number of crashes vs page and percent crashes vs page bar graphs

--- public/d3Funcs.js
+++ public/d3Funcs.js
@@ -4,6 +4,7 @@
 //showScatterPlotAll(); //shows all the pages's plots
 //showBarGraph();
 showBarGraphNumFails();
+showBarGraphPercentFails();
 
 function showScatterPlot(page) {
     // just to have some space around items. 
@@ -285,6 +286,7 @@ data = freqTotal;
 
 }
 
+
 //bar graph for #fails vs page
 
 function showBarGraphNumFails() {
@@ -322,13 +324,13 @@ var svg = d3.select("#bar-graph-num-fails").append("svg")
 // load the data
 d3.json("data.json", function(error, data) {
 
-var freqTotal = d3.nest()
-  .key(function(d) { return d.Letter; })
-  .rollup(function(v) { return d3.sum(v, function(d) { return d.test==true; }); })
+var numCrashes = d3.nest()
+  .key(function(d) { return d.current_page; })
+  .rollup(function(v) { return d3.sum(v, function(d) { return d.did_aww_snap==true; }); })
   .entries(data);
-  console.log(JSON.stringify(freqTotal));
+  console.log(JSON.stringify(numCrashes));
 
-data = freqTotal;
+data = numCrashes;
 
   // scale the range of the data
   x.domain(data.map(function(d) { return d.key; }));
@@ -353,7 +355,92 @@ data = freqTotal;
       .attr("y", -margin.left)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Average bytes used");
+      .text("Number of Crashes");
+
+  // Add bar chart
+  svg.selectAll("bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.key); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.values); })
+      .attr("height", function(d) { return height - y(d.values); });
+
+});
+
+}
+
+
+//bar graph for %fails vs page
+
+function showBarGraphPercentFails() {
+ var margin = {top: 20, right: 20, bottom: 70, left: 100},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+
+// set the ranges
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+// define the axis
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+
+// add the SVG element
+var svg = d3.select("#bar-graph-percent-fails").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+// load the data
+d3.json("data.json", function(error, data) {
+
+var percentCrashes = d3.nest()
+  .key(function(d) { return d.current_page; })
+  .rollup(function(v) { return (d3.sum(v, function(d) { return d.did_aww_snap==true; }))/v.length; })
+  .entries(data);
+  console.log(JSON.stringify(percentCrashes));
+
+data = percentCrashes;
+
+  // scale the range of the data
+  x.domain(data.map(function(d) { return d.key; }));
+  y.domain([0, d3.max(data, function(d) { return d.values; })]);
+
+  // render axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margin.left)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("percent Crashes");
 
   // Add bar chart
   svg.selectAll("bar")
